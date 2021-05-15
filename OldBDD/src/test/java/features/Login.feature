216da4c455be156d8496dbe27a544bfe352c3a00
Umang <umang@test.com>
2021-05-15 17:00:03 +0530

    BDD Practice

---
+++ OldBDD/src/test/java/features/Login.feature
@@ -0,0 +1,5 @@
+Feature: My feature file for practice
+
+  Scenario: My Test Scenario
+    Given Launch URL
+    And Login to Application
---
+++ OldBDD/src/test/java/loginstep.java
@@ -0,0 +1,17 @@
+//import cucumber.api.java.en.And;
+//import cucumber.api.java.en.Given;
+
+
+//import io.cucumber.java.en.*;
+
+import cucumber.api.java.en.*;
+
+public class loginstep {
+    @Given("^Launch URL$")
+    public void launchURL() {
+    }
+
+    @And("^Login to Application$")
+    public void loginToApplication() {
+    }
+}
